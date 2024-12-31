ab6d8c9224abf132139fc83aeffd4e81ea705fe1
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-12-30 18:09:07 -0600

    Update route.ts

--- app/webhook/route.ts
+++ app/webhook/route.ts
@@ -1,4 +1,4 @@
-export const GET = async (request: Request) => {
+export const POST = async (request: Request) => {
 	const body = await request.text();
 	console.log(body);
 	return new Response(body, { status: 200 });
